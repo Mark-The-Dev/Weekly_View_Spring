e3c93498d33e00f43113b4351f5052a98a2c8e4e
Mark <SwiftTactics@gmail.com>
2021-04-25 16:34:41 -0400

    added delete route, fixed up mapping annotations to be correct

--- src/main/java/com/markthedev/weekly_view/controller/UsersController.java
+++ src/main/java/com/markthedev/weekly_view/controller/UsersController.java
@@ -8,7 +8,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/")
@@ -35,14 +37,14 @@ public class UsersController {
 
     }
     // save user
-    @GetMapping("users")
+    @PostMapping("users")
     public Users createUser(@RequestBody Users user){
         return this.usersRepository.save(user);
 
     }
 
     // update user
-
+    @PatchMapping("users/{id}")
     public ResponseEntity<Users> updateUser(@PathVariable(value ="id") Long userId, @Valid @RequestBody Users userDetails) throws ResourceNotFoundException{
         Users user = usersRepository.findById(userId)
                 .orElseThrow(() -> new ResourceNotFoundException("User not found by this id: " + userId));
@@ -53,5 +55,17 @@ public class UsersController {
         return ResponseEntity.ok(this.usersRepository.save(user));
     }
 
+    @DeleteMapping("users/{id}")
     // delete user
+    public Map<String, Boolean> deleteUser(@PathVariable(value = "id") Long userId) throws ResourceNotFoundException{
+        Users user = usersRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found by this id: " + userId));
+        this.usersRepository.delete(user);
+
+        Map<String, Boolean> responce = new HashMap<>();
+        responce.put("deleted", Boolean.TRUE);
+
+        return responce;
+
+    }
 }
