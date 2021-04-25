78d0bc2a94a0b68bb734310ab646fd8204052c7d
Mark <SwiftTactics@gmail.com>
2021-04-25 06:25:42 -0400

    initial commit

---
+++ src/main/java/com/markthedev/weekly_view/WeeklyViewApplication.java
@@ -0,0 +1,13 @@
+package com.markthedev.weekly_view;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeklyViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeeklyViewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/markthedev/weekly_view/WeeklyViewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markthedev.weekly_view;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeeklyViewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
